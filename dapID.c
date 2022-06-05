853d07032a497ba6d4ee6b0de6ebbdd289488a77
edaly <eddaly@aol.com>
2022-06-04 21:24:00 -0400

    updated createid script

--- dapID.c
+++ dapID.c
@@ -3,4 +3,7 @@
 // It contains creation data that will be embedded in the dap library
 // Include in dapinit function so it also gets included in application
 void dapID(void) {
-	const char dapBranchID[] = "DAP, Gen 1, Branch = main";	const char dapLocalSHA[] = "DAP, Gen 1, SHA = c3bc4d0dba267394e2b65a3759b505fa3cc3ab54";	return;}
+	const char dapBranchID[] = "DAP, Gen 1, Branch = main";
+	const char dapLocalSHA[] = "DAP, Gen 1, SHA = a52c2b5f5ffb27cf8958d4078329f03daf7eba14";
+	return;
+}
