cba7b23477c96aae436c744c262ad1396a16be0f
edaly <eddaly@aol.com>
2022-06-05 06:38:10 -0400

    added createid1

--- dapID.c
+++ dapID.c
@@ -1,9 +1,8 @@
 // dapID.c
-// This an auto-generated file, do not edit
+// This an AUTO-GENERATED file, DO NOT EDIT
 // It contains creation data that will be embedded in the dap library
-// Include in dapinit function so it also gets included in application
-void dapID(void) {
-	const char dapBranchID[] = "DAP, Gen 1, Branch = main";
-	const char dapLocalSHA[] = "DAP, Gen 1, SHA = a52c2b5f5ffb27cf8958d4078329f03daf7eba14";
-	return;
+// Include in dapinit function so it also gets included in the application
+const char dap_id[] = "DAP: Date(Sun 05 Jun 2022 06:30:44 AM EDT), Branch(main), SHA(853d07032a497ba6d4ee6b0de6ebbdd289488a77)";
+char *dapID(void) {
+	return dap_id;
 }
