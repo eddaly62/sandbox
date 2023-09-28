bd6b29717ad9737923f9032ad71b5d9be2cadfe4
edaly <eddaly@aol.com>
2023-09-27 20:10:38 -0400

    conf.c changes

--- conf.c
+++ conf.c
@@ -6,11 +6,6 @@
 #include <string.h>
 #include <assert.h>
 
-typedef struct cf {
-    int fd;
-    char so[10][100];
-} CONF;
-
 
 int conf_parse_line(char *si, char *so[]) {
 
@@ -19,28 +14,29 @@ int conf_parse_line(char *si, char *so[]) {
     char *token;
     char *sic, *sicc;
 
-    if (si[0] == '#') {
-        // line is a comment
-        return 0;
-    }
-
     // strtok is destructive on input string so make a duplicate
     i = 0;
     sic = strdup(si);
     sicc = sic;
 
+    if ((si[0] == '#') || (si[0] == ' ')) {
+        // line is a comment or blank
+        return 0;
+    }
+
     // parse string
     while (token != NULL) {
         if (i == 0) {
-            token = strtok_r(sic, ",= ", &sicc);
+            token = strtok_r(sic, " ,=\n", &sicc);
         }
         else {
-            token = strtok_r(NULL, ",= ", &sicc);
+            token = strtok_r(NULL, " ,=\n", &sicc);
         }
 
         if (token != NULL) {
-            memcpy(so[i], token, strlen(token));
-            printf("so[%d] = %s\n", i, so[i]);
+            strcpy(so[i], token);
+            printf("length of token = %ld\n", strlen(token));
+            printf("so[%d](%s), token(%s)\n", i, so[i], token);
             i++;
         }
     }
@@ -50,12 +46,23 @@ int conf_parse_line(char *si, char *so[]) {
 }
 
 // config setting
-int conf_set(char *sarg[]){
+int conf_set(char *so[]){
 
-    int r;
-    r = memcmp("fg", sarg[0], strlen(sarg[0]));
-    if(r == 0){
-        printf("setting %s\n",sarg[0]);
+    // sample seting logic
+    if(memcmp("fc", so[0], strlen("fc")) == 0){
+        printf("setting %s\n",so[0]);
+        return 0;
+    }
+    else if(memcmp("fb", so[0], strlen("fb")) == 0){
+        printf("setting %s\n",so[0]);
+        return 0;
+    }
+    else if(memcmp("vres", so[0], strlen("vres")) == 0){
+        printf("setting %s\n",so[0]);
+        return 0;
+    }
+    else if(memcmp("hres", so[0], strlen("hres")) == 0){
+        printf("setting %s\n",so[0]);
         return 0;
     }
     else {
@@ -64,44 +71,57 @@ int conf_set(char *sarg[]){
     }
 }
 
+#define MAX_LINE 128
+int conf_read_file(char *pathname, char *so[]) {
+
+    assert(pathname != NULL);
+    int r;
+    int len;
+    FILE *fd;
+    char si[100];
+
+    // Open the file for reading
+    fd = fopen (pathname, "r");
+	if (fd == NULL) {
+		fprintf(stderr, "Error: cannot open configure file(%s) in function(%s)\n", pathname, __FUNCTION__);
+		return -1;
+    }
+
+    while (fgets(si, MAX_LINE, fd) != NULL) {
+
+        if (strlen(si) > 1) {
+            // not a blank line
+            printf("readin %s\n", si);
+
+            len = conf_parse_line(si, so);
+            printf("len = %d\n", len);
+
+            if (len > 1) {
+                conf_set(so);
+            }
+        }
+
+    }
+
+    fclose(fd);
+}
+
 int main(int argc, char *argv[])
 {
 
     char *so[10];
-    char *s = "fg = 123, 234, 345";
-    int len;
+    //int len;
     int n;
-    CONF cf;
+    int r;
 
     for (n=0; n<10; n++){
         so[n] = (char *)malloc(sizeof(char)*100);
     }
 
-    len = conf_parse_line(s, so);
-    printf("len = %d\n", len);
-
-    if (len > 1) {
-        conf_set(so);
+    r = conf_read_file("confdata.conf", so);
+    if (r == -1){
+        printf("could not open confdata.conf file\n");
     }
-
-#if 0
-    if (argc != 2) {
-		printf("Usage: ./fbinfo /dev/fb0\n");
-		exit(1);
-	}
- 
-	if (strstr(argv[1], "fb") == NULL) {
-		printf("Only frame buffers (fb) are supported with this example\n");
-		exit(1);
-	}		
-
-    // Open the file for reading and writing
-	fbfd = open(argv[1], O_RDWR);
-	if (fbfd == -1) {
-		perror("Error: cannot open framebuffer device");
-		goto handle_allocate_error;
-	}
-#endif
 
     for (n=0; n<10; n++){
         if (so[n] != NULL) {
