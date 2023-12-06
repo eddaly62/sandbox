acc606ff8b111753db68afa6f30be277921f62ea
Ed Daly <edaly@generaldigital.com>
2023-12-06 16:35:55 -0500

    new programs added

---
+++ get_process_name.c
@@ -0,0 +1,66 @@
+// get_process_name.c
+// this program gets the process name given the process ID
+
+#include <pthread.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <ctype.h>
+#include <assert.h>
+
+int get_process_name_by_pid(int pid, char *name, size_t len)
+{
+    FILE *f;
+    size_t size;
+
+    assert(name != NULL);
+    assert(len > 0);
+
+    if(name != NULL){
+        sprintf(name, "/proc/%d/cmdline", pid);
+        f = fopen(name, "r");
+        if(f != NULL) {
+            size = fread(name, sizeof(char), len, f);
+            printf("name = (%s)\n", name);
+            if(size > 0) {
+                if('\n' == name[size-1])
+                    name[size-1]='\0';
+            }
+            fclose(f);
+            printf("size = %ld\n", size);
+            return (int)size;
+        }
+        else {
+            // could not open file
+            return -1;
+        }
+    }
+    else {
+        // error
+        return -1;
+    }
+}
+
+
+int main(int argc, char* argv[]) {
+
+    int r;
+    char name[1024];
+
+    if (argc != 2) {
+        printf("Usage:\n\t%s <PID>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    r = get_process_name_by_pid(atoi(argv[1]), name, sizeof(name));
+    if (r == -1) {
+        printf("Could not resolve name of pid = %s\n", argv[1]);
+    }
+    else {
+        printf("PID(%s) is (%s), size of name(%d)\n", argv[1], name, r);
+    }
+
+    exit(EXIT_SUCCESS);
+}
---
+++ itimer.c
@@ -0,0 +1,86 @@
+// itimer.h
+
+#include <time.h>
+#include <stdio.h>
+#include <signal.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <errno.h>
+
+
+       #include <signal.h>
+       #include <stdint.h>
+       #include <stdio.h>
+       #include <stdlib.h>
+       #include <time.h>
+       #include <unistd.h>
+
+//#define CLOCKID CLOCK_REALTIME
+#define SIG SIGRTMIN
+
+           timer_t            timerid;
+           sigset_t           mask;
+           long long          freq_nanosecs;
+           struct sigevent    sev;
+           struct sigaction   sa;
+           struct itimerspec  its;
+
+
+
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
+#include <time.h>
+
+
+static int i = 0;
+
+//Thread function to be invoked when the periodic timer expires
+void sighler (union sigval val)
+{
+    printf("Handler entered with value :%d for %d times\n", val.sival_int, ++i);
+}
+
+int main()
+{
+    int Ret;
+    pthread_attr_t attr;
+    pthread_attr_init( &attr );
+    struct sched_param parm;
+    parm.sched_priority = 255;
+    pthread_attr_setschedparam(&attr, &parm);
+    struct sigevent sig;
+
+    sig.sigev_notify = SIGEV_THREAD;
+    sig.sigev_notify_function = sighler;
+    sig.sigev_value.sival_int =20;
+    sig.sigev_notify_attributes = &attr;
+
+    //create a new timer.
+    timer_t timerid;
+
+    Ret = timer_create(CLOCK_REALTIME, &sig, &timerid);
+    if (Ret == 0)
+    {
+        struct itimerspec in, out;
+        in.it_value.tv_sec = 1;
+        in.it_value.tv_nsec = 0;
+        in.it_interval.tv_sec = 0;
+        in.it_interval.tv_nsec = 100000000;
+        //issue the periodic timer request here.
+        Ret = timer_settime(timerid, 0, &in, &out);
+        if(Ret == 0)
+            sleep(2);
+        else
+            printf("timer_settime() failed with %d\n", errno);
+        //delete the timer.
+        timer_delete(timerid);
+    }
+    else
+    {
+    printf("timer_create() failed with %d\n", errno);
+    return Ret;
+    }
+}
