4680e2418f81b98fd492cd023be9922053a5d781
Ed Daly <edaly@generaldigital.com>
2024-01-10 09:14:00 -0500

    itemer2 changes

--- itimer2.c
+++ itimer2.c
@@ -8,7 +8,7 @@
 #include <errno.h>
 #include <stdlib.h>
 
-static int i = 0;
+int i = 0;
 
 typedef struct it {
     pthread_attr_t attr;
@@ -18,39 +18,40 @@ typedef struct it {
 } INT_TIMER;
 
 INT_TIMER itmr = {
-    .in.it_value.tv_sec = 1,
+    .in.it_value.tv_sec = 10,
     .in.it_value.tv_nsec = 0,
     .in.it_interval.tv_sec = 1,
     .in.it_interval.tv_nsec = 0,
     .sig.sigev_notify = SIGEV_THREAD,
-    .sig.sigev_value.sival_int = 21,
+    .sig.sigev_value.sival_int = 25,
     .sig.sigev_notify_attributes = &itmr.attr,
 };
+
+
+// create and start a timer
+int interval_timer(INT_TIMER *itmr, void *func) {
+
+    int r;
+
+    pthread_attr_init(&itmr->attr);
+    itmr->sig.sigev_notify_function = func;
+
+    r = timer_create(CLOCK_REALTIME, &itmr->sig, &itmr->timerid);
+    if (r == 0) {
+
+        // issue the periodic timer request here.
+        r = timer_settime(itmr->timerid, 0, &itmr->in, &itmr->out);
+        if(r != 0) {
+            r = timer_delete(itmr->timerid);
+        }
+    }
+    return r;
+}
 
 // Thread function to be invoked when the periodic timer expires
 void sighler (union sigval val)
 {
     printf("Handler entered with value :%d for %d times\n", val.sival_int, ++i);
-}
-
-// initialize and start a timer
-int interval_timer(INT_TIMER *itmr, void *func) {
-
-    int ret;
-
-    pthread_attr_init(&itmr->attr);
-    itmr->sig.sigev_notify_function = func;
-
-    ret = timer_create(CLOCK_REALTIME, &itmr->sig, &itmr->timerid);
-    if (ret == 0) {
-
-        // issue the periodic timer request here.
-        ret = timer_settime(itmr->timerid, 0, &itmr->in, &itmr->out);
-        if(ret != 0) {
-            ret = timer_delete(itmr->timerid);
-        }
-    }
-    return ret;
 }
 
 int main()
@@ -60,7 +61,7 @@ int main()
     ret = interval_timer(&itmr, sighler);
 
     if(ret == 0)
-        sleep(11);
+        sleep(20);
     else
         printf("interval_timer() failed with %d\n", errno);
 }
